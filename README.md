4bfc6632a4be4f013ef332095fb888d0159a949c
ᴍʀ ɴɪᴍᴀ <98468832+Mrnimama@users.noreply.github.com>
2024-10-03 02:41:52 +0000

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # pixaldrain-sinhalasub
-Idk
+
+## How to use.
+1. Copy This code and make file `pixaldrain-sinhslasub.js` 
+
+### 1. Get all direct  links with quality.
+```
+var opts = {
+    link: "https://sinhalasub.lk/movies/the-greatest-of-all-time-2024-sinhala-subtitles/",
+    quality: "HD 720p",
+    type: "alllinks" // "direct"
+}
+PixaldrainDL(opts)
+```
+### Result
+```
+{
+  '4K 2160p': 'https://pixeldrain.com/api/file/nFP81WRg',
+  'FHD 1080p': 'https://pixeldrain.com/api/file/8YQrJ2yn',
+  'HD 720p': 'https://pixeldrain.com/api/file/nDM7ERPW',
+  'SD 480p': 'https://pixeldrain.com/api/file/p6nmr7Vk'
+}
+```
+</br>
+
+### 2. Get only one direct link is you given quality.
+```
+var opts = {
+    link: "https://sinhalasub.lk/movies/the-greatest-of-all-time-2024-sinhala-subtitles/",
+    quality: "HD 720p",
+    type: "direct" // "alllinks"
+}
+PixaldrainDL(opts)
+```
+### Result
+```
+https://pixeldrain.com/api/file/nDM7ERPW
+```
--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const { sinhalaSub } = require("mrnima-PixaldrainDL");
+const { sinhalaSub } = require("mrnima-moviedl");
 
 /**
  * Pixaldrain download 
  * @param {*} opts 
  */
 async function PixaldrainDL(opts) {
-     var movie = await sinhalaSub();
+    var movie = await sinhalaSub();
 
     var link = opts.link; // Put here to movie link or episode link
-    var {result} = await movie.download(link);
+    var { result } = await movie.download(link);
     var pixeldrain = {};
-    for(let index of result.links){
-        if(index.link.includes("https://pixeldrain.com/")){
-            pixeldrain[index.quality] = index.link.replace("/u/","/api/file/")
+    for (let index of result.links) {
+        if (index.link.includes("https://pixeldrain.com/")) {
+            pixeldrain[index.quality] = index.link.replace("/u/", "/api/file/")
         }
     }
 
     var directLink = pixeldrain[opts.quality]
     console.log(pixeldrain)
     console.log(directLink)
-    return directLink;
+    return opts.type === "direct" ? directLink : opts.type === "alllinks" ? pixeldrain : "Give type : direct or alllinks" ;
 }
 /*
 module.exports = {
@@ -31,7 +31,8 @@ module.exports = {
 //Example
 // var {PixaldrainDL} = require("./file...")
 var opts = {
-    link: "https://sinhalasub.lk/movies/deadpool-wolverine-2024-sinhala-subtitles/",
-    quality:"HD 720p",
+    link: "https://sinhalasub.lk/movies/the-greatest-of-all-time-2024-sinhala-subtitles/",
+    quality: "HD 720p",
+    type: "direct" // "alllinks"
 }
 PixaldrainDL(opts)
